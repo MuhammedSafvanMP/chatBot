d8815aeae9678bbffd504faa90c51528f3b7f885
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2023-10-30 19:05:49 +0530

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
-
 export default function App() {
   return (
-    <div>App</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }

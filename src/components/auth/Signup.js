3fc5d74172b58bfb4bcca25d0de9948b0f1d5fc6
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2023-10-31 12:08:46 +0530

    added responsive signup.js

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 const Signup = () => {
 
   return (
-    <section className="flex justify-center items-center">
+    <section className="flex justify-center items-center p-5 md:p-0">
       <img src="./img/log_chatgpt.svg" alt="chatGPT-logo" className="w-7 absolute top-9" />
       <div className="h-screen w-screen flex flex-col justify-between items-center pt-52">
         <div className="flex flex-col gap-3 text-center w-96">
-          <h2 className="text-4xl font-bold">Create your account</h2>
+          <h2 className="text-2xl font-bold md:ext-4xl">Create your account</h2>
           <p className='pb-5 text-sm'>Note that phone verification may be required for <br /> signup. Your number will only be used to verify your <br /> identity for security purposes.</p>
-          <div className="relative">
-            <input type="email" className="input border-2 rounded-lg pl-4 py-4 w-full max-w-6xl outline-none" />
-            <label className="placeholder absolute top-4 left-4 pointer-events-none transition delay-75  text-gray-500">Email address</label>
+          <div className="relative p-5 md:p-0">
+            <input type="email" className="input border-2 rounded-lg pl-3 py-3 w-full max-w-6xl outline-none md:pl-4 md:py-4" />
+            <label className="placeholder absolute top-8 left-10 pointer-events-none transition delay-75  text-gray-500 md:top-4 md:left-4">Email address</label>
           </div>
-          <Link to='/signpass' className="py-4 mt-5 rounded-lg text-white bg-alg_btn">
+          <Link to='/signpass' className="py-3 mx-5 rounded-lg text-white bg-alg_btn md:py-4 md:mt-5 md:mx-0">
             Continue 
           </Link>
           <p>
@@ -24,20 +24,20 @@ const Signup = () => {
             <span className="mx-4 text-xs">OR</span>
             <div className="w-2/3 border-t border-gray-400"></div>
           </div>
-          <div className="flex gap-4 pl-4 py-4 border-2 rounded-lg border-gray-300">
+          <div className="flex gap-4 mx-5 pl-4 py-4 border-2 rounded-lg border-gray-300 md:mx-0">
             <img src="./img/google.png" alt="" className="w-5" />
             Continue with Google
           </div>
-          <div className="flex gap-4 pl-4 py-4 border-2 rounded-lg border-gray-300">
+          <div className="flex gap-4 mx-5 pl-4 py-4 border-2 rounded-lg border-gray-300 md:mx-0">
             <img src="./img/micro.png" alt="" className="w-5" />
             Continue with Microsoft Account
           </div>
-          <div className="flex gap-4 pl-4 items-center py-4 border-2 rounded-lg border-gray-300">
+          <div className="flex gap-4 mx-5 pl-4 items-center py-4 border-2 rounded-lg border-gray-300 md:mx-0">
             <i className="fa-brands fa-apple text-xl"></i> Continue with Apple
           </div>
         </div>
 
-        <footer className="pb-10 text-alg_btn">
+        <footer className="pb-3 pt-10 text-alg_btn md:pb-10">
           <div className="flex gap-4 text-sm">
             <span>Terms of use</span>|<span>Privacy Policy</span>
           </div>
